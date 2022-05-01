66475d4e470650ec116d63daa0377f61f3c492a1
Carlos Aguilar <carl.aguilar.l@gmail.com>
2022-05-01 00:07:50 -0400

    Primera versión funcional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import {BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Report from './pages/Report';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Empty!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={ <Home/> }/>
+          <Route path='/products' element={ <Products/> }/>
+          <Route path='/report' element={ <Report/> }/>          
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
